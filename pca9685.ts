63d3fcb008aed0fe50601043c8c0767aad8cf94d
wenjie <skyerider777@hotmail.com>
2021-07-31 09:45:31 +0800

    first commit

---
+++ pca9685.ts
@@ -0,0 +1,379 @@
+/**
+ * PCA9685
+ */
+//% weight=100 color=#0fbc11 icon=""
+//% color="#794044" weight=10 icon="\uf108" block="PCA9685舵机模块"
+//% category="PCA9685舵机模块"
+namespace PCA9685 {
+    let _DEBUG: boolean = false
+    const debug = (msg: string) => {
+        if (_DEBUG === true) {
+            serial.writeLine(msg)
+        }
+    }
+
+    const MIN_CHIP_ADDRESS = 0x40
+    const MAX_CHIP_ADDRESS = MIN_CHIP_ADDRESS + 62
+    const chipResolution = 4096;
+    const PrescaleReg = 0xFE //the prescale register address
+    const modeRegister1 = 0x00 // MODE1
+    const modeRegister1Default = 0x01
+    const modeRegister2 = 0x01 // MODE2
+    const modeRegister2Default = 0x04
+    const sleep = modeRegister1Default | 0x10; // Set sleep bit to 1
+    const wake = modeRegister1Default & 0xEF; // Set sleep bit to 0
+    const restart = wake | 0x80; // Set restart bit to 1
+    const allChannelsOnStepLowByte = 0xFA // ALL_LED_ON_L
+    const allChannelsOnStepHighByte = 0xFB // ALL_LED_ON_H
+    const allChannelsOffStepLowByte = 0xFC // ALL_LED_OFF_L
+    const allChannelsOffStepHighByte = 0xFD // ALL_LED_OFF_H
+    const PinRegDistance = 4
+    const channel0OnStepLowByte = 0x06 // LED0_ON_L
+    const channel0OnStepHighByte = 0x07 // LED0_ON_H
+    const channel0OffStepLowByte = 0x08 // LED0_OFF_L
+    const channel0OffStepHighByte = 0x09 // LED0_OFF_H
+
+    const hexChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+
+     export enum PinNum {
+        Pin0 = 0,
+        Pin1 = 1,
+        Pin2 = 2,
+        Pin3 = 3,
+        Pin4 = 4,
+        Pin5 = 5,
+        Pin6 = 6,
+        Pin7 = 7,
+        Pin8 = 8,
+        Pin9 = 9,
+        Pin10 = 10,
+        Pin11 = 11,
+        Pin12 = 12,
+        Pin13 = 13,
+        Pin14 = 14,
+        Pin15 = 15,
+    }
+
+    export class ServoConfigObject {
+        id: number;
+        pinNumber: number;
+        minOffset: number;
+        midOffset: number;
+        maxOffset: number;
+        position: number;
+    }
+
+    export const DefaultServoConfig = new ServoConfigObject();
+    DefaultServoConfig.pinNumber = -1
+    DefaultServoConfig.minOffset = 5
+    DefaultServoConfig.midOffset = 15
+    DefaultServoConfig.maxOffset = 25
+    DefaultServoConfig.position = 90
+
+    export class ServoConfig {
+        id: number;
+        pinNumber: number;
+        minOffset: number;
+        midOffset: number;
+        maxOffset: number;
+        position: number;
+        constructor(id: number, config: ServoConfigObject) {
+            this.id = id
+            this.init(config)
+        }
+
+        init(config: ServoConfigObject) {
+            this.pinNumber = config.pinNumber > -1 ? config.pinNumber : this.id - 1
+            this.setOffsetsFromFreq(config.minOffset, config.maxOffset, config.midOffset)
+            this.position = -1
+        }
+
+        debug() {
+            const params = this.config()
+
+            for (let j = 0; j < params.length; j = j + 2) {
+                debug(`Servo[${this.id}].${params[j]}: ${params[j + 1]}`)
+            }
+        }
+
+        setOffsetsFromFreq(startFreq: number, stopFreq: number, midFreq: number = -1): void {
+            this.minOffset = startFreq // calcFreqOffset(startFreq)
+            this.maxOffset = stopFreq // calcFreqOffset(stopFreq)
+            this.midOffset = midFreq > -1 ? midFreq : ((stopFreq - startFreq) / 2) + startFreq
+        }
+
+        config(): string[] {
+            return [
+                'id', this.id.toString(),
+                'pinNumber', this.pinNumber.toString(),
+                'minOffset', this.minOffset.toString(),
+                'maxOffset', this.maxOffset.toString(),
+                'position', this.position.toString(),
+            ]
+        }
+    }
+
+    export class ChipConfig {
+        address: number;
+        servos: ServoConfig[];
+        freq: number;
+        constructor(address: number = 0x40, freq: number = 50) {
+            this.address = address
+            this.servos = [
+                new ServoConfig(1, DefaultServoConfig),
+                new ServoConfig(2, DefaultServoConfig),
+                new ServoConfig(3, DefaultServoConfig),
+                new ServoConfig(4, DefaultServoConfig),
+                new ServoConfig(5, DefaultServoConfig),
+                new ServoConfig(6, DefaultServoConfig),
+                new ServoConfig(7, DefaultServoConfig),
+                new ServoConfig(8, DefaultServoConfig),
+                new ServoConfig(9, DefaultServoConfig),
+                new ServoConfig(10, DefaultServoConfig),
+                new ServoConfig(11, DefaultServoConfig),
+                new ServoConfig(12, DefaultServoConfig),
+                new ServoConfig(13, DefaultServoConfig),
+                new ServoConfig(14, DefaultServoConfig),
+                new ServoConfig(15, DefaultServoConfig),
+                new ServoConfig(16, DefaultServoConfig)
+            ]
+            this.freq = freq
+            init(address, freq)
+        }
+    }
+
+    export const chips: ChipConfig[] = []
+
+    function calcFreqPrescaler(freq: number): number {
+        return (25000000 / (freq * chipResolution)) - 1;
+    }
+
+    function stripHexPrefix(str: string): string {
+        if (str.length === 2) {
+            return str
+        }
+        if (str.substr(0, 2) === '0x') {
+            return str.substr(-2, 2)
+        }
+        return str
+    }
+
+    function write(chipAddress: number, register: number, value: number): void {
+        const buffer = pins.createBuffer(2)
+        buffer[0] = register
+        buffer[1] = value
+        pins.i2cWriteBuffer(chipAddress, buffer, false)
+    }
+
+    export function getChipConfig(address: number): ChipConfig {
+        for (let i = 0; i < chips.length; i++) {
+            if (chips[i].address === address) {
+                debug(`Returning chip ${i}`)
+                return chips[i]
+            }
+        }
+        debug(`Creating new chip for address ${address}`)
+        const chip = new ChipConfig(address)
+        const index = chips.length
+        chips.push(chip)
+        return chips[index]
+    }
+
+    function calcFreqOffset(freq: number, offset: number) {
+        return ((offset * 1000) / (1000 / freq) * chipResolution) / 10000
+    }
+
+    /**
+     * Used to set the pulse range (0-4095) of a given pin on the PCA9685
+     * @param chipAddress [64-125] The I2C address of your PCA9685; eg: 64
+     * @param pinNumber The pin number (0-15) to set the pulse range on
+     * @param onStep The range offset (0-4095) to turn the signal on
+     * @param offStep The range offset (0-4095) to turn the signal off
+     */
+    //% block="设置模块引脚 $pinNumber|的脉宽范围，通=$onStep|，断=$offStep|，地址=$chipAddress" group="PCA9685"  advanced=true
+    export function setPinPulseRange(pinNumber: PinNum = 0, onStep: number = 0, offStep: number = 2048, chipAddress: number = 0x40): void {
+        pinNumber = Math.max(0, Math.min(15, pinNumber))
+        const buffer = pins.createBuffer(2)
+        const pinOffset = PinRegDistance * pinNumber
+        onStep = Math.max(0, Math.min(4095, onStep))
+        offStep = Math.max(0, Math.min(4095, offStep))
+
+        debug(`setPinPulseRange(${pinNumber}, ${onStep}, ${offStep}, ${chipAddress})`)
+        debug(`  pinOffset ${pinOffset}`)
+
+        // Low byte of onStep
+        write(chipAddress, pinOffset + channel0OnStepLowByte, onStep & 0xFF)
+
+        // High byte of onStep
+        write(chipAddress, pinOffset + channel0OnStepHighByte, (onStep >> 8) & 0x0F)
+
+        // Low byte of offStep
+        write(chipAddress, pinOffset + channel0OffStepLowByte, offStep & 0xFF)
+
+        // High byte of offStep
+        write(chipAddress, pinOffset + channel0OffStepHighByte, (offStep >> 8) & 0x0F)
+    }
+
+    /**
+     * Used to set the duty cycle (0-100) of a given led connected to the PCA9685
+     * @param chipAddress [64-125] The I2C address of your PCA9685; eg: 64
+     * @param ledNumber The number (1-16) of the LED to set the duty cycle on
+     * @param dutyCycle The duty cycle (0-100) to set the LED to
+     */
+    //% block="设置模块引脚 $ledNum|的占空比为 $dutyCycle|，地址=$chipAddress" group="PCA9685"  advanced=true
+    export function setLedDutyCycle(ledNum: PinNum = 0, dutyCycle: number, chipAddress: number = 0x40): void {
+        ledNum = Math.max(0, Math.min(15, ledNum))
+        dutyCycle = Math.max(0, Math.min(100, dutyCycle))
+        const pwm = (dutyCycle * (chipResolution - 1)) / 100
+        return setPinPulseRange(ledNum, 0, pwm, chipAddress)
+    }
+
+    function degrees180ToPWM(freq: number, degrees: number, offsetStart: number, offsetEnd: number): number {
+        // Calculate the offset of the off point in the freq
+        offsetEnd = calcFreqOffset(freq, offsetEnd)
+        offsetStart = calcFreqOffset(freq, offsetStart)
+        const spread: number = offsetEnd - offsetStart
+        const calcOffset: number = ((degrees * spread) / 180) + offsetStart
+        // Clamp it to the bounds
+        return Math.max(offsetStart, Math.min(offsetEnd, calcOffset))
+    }
+
+    /**
+     * Used to move the given servo to the specified degrees (0-180) connected to the PCA9685
+     * @param chipAddress [64-125] The I2C address of your PCA9685; eg: 64
+     * @param servoNum The number (1-16) of the servo to move
+     * @param degrees The degrees (0-180) to move the servo to
+     */
+    //% block="将$servoNum|号引脚上的180度舵机转到角度 $degrees|，地址=$chipAddress" group="PCA9685" 
+    export function setServoPosition(servoNum: PinNum = 0, degrees: number, chipAddress: number = 0x40): void {
+        const chip = getChipConfig(chipAddress)
+        servoNum = Math.max(0, Math.min(15, servoNum))
+        degrees = Math.max(0, Math.min(180, degrees))
+        const servo: ServoConfig = chip.servos[servoNum]
+        const pwm = degrees180ToPWM(chip.freq, degrees, servo.minOffset, servo.maxOffset)
+        servo.position = degrees
+        debug(`setServoPosition(${servoNum}, ${degrees}, ${chipAddress})`)
+        debug(`  servo.pinNumber ${servo.pinNumber}`)
+        debug(`  servo.minOffset ${servo.minOffset}`)
+        debug(`  servo.maxOffset ${servo.maxOffset}`)
+        debug(`  pwm ${pwm}`)
+        servo.debug()
+        return setPinPulseRange(servo.pinNumber, 0, pwm, chipAddress)
+    }
+
+    /**
+     * Used to set the rotation speed of a continous rotation servo from -100% to 100%
+     * @param chipAddress [64-125] The I2C address of your PCA9685; eg: 64
+     * @param servoNum The number (1-16) of the servo to move
+     * @param speed [-100-100] The speed (-100-100) to turn the servo at
+     */
+    //% block="将$servoNum|号引脚上的360度舵机转到角度 $degrees|，地址=$chipAddress" group="PCA9685" 
+    export function setCRServoPosition(servoNum: PinNum = 0, speed: number, chipAddress: number = 0x40): void {
+        const chip = getChipConfig(chipAddress)
+        const freq = chip.freq
+        servoNum = Math.max(0, Math.min(15, servoNum))
+        const servo: ServoConfig = chip.servos[servoNum]
+        const offsetStart = calcFreqOffset(freq, servo.minOffset)
+        const offsetMid = calcFreqOffset(freq, servo.midOffset)
+        const offsetEnd = calcFreqOffset(freq, servo.maxOffset)
+        if (speed === 0) {
+            return setPinPulseRange(servo.pinNumber, 0, offsetMid, chipAddress)
+        }
+        const isReverse: boolean = speed < 0
+        debug(isReverse ? 'Reverse' : 'Forward')
+        const spread = isReverse ? offsetMid - offsetStart : offsetEnd - offsetMid
+        debug(`Spread ${spread}`)
+        servo.position = speed
+        speed = Math.abs(speed)
+        const calcOffset: number = ((speed * spread) / 100)
+        debug(`Offset ${calcOffset}`)
+        debug(`min ${offsetStart}`)
+        debug(`mid ${offsetMid}`)
+        debug(`max ${offsetEnd}`)
+        const pwm = isReverse ? offsetMid - calcOffset : offsetMid + calcOffset
+        debug(`pwm ${pwm}`)
+        return setPinPulseRange(servo.pinNumber, 0, pwm, chipAddress)
+    }
+
+    /**
+     * Used to set the range in centiseconds (milliseconds * 10) for the pulse width to control the connected servo
+     * @param chipAddress [64-125] The I2C address of your PCA9685; eg: 64
+     * @param servoNum The number (1-16) of the servo to move; eg: 1
+     * @param minTimeCs The minimum centiseconds (0-1000) to turn the servo on; eg: 5
+     * @param maxTimeCs The maximum centiseconds (0-1000) to leave the servo on for; eg: 25
+     * @param midTimeCs The mid (90 degree for regular or off position if continuous rotation) for the servo; eg: 15
+     */
+    //% block="将$servoNum|号引脚上的舵机脉宽限制为：最小$minTimeCs|，最大=$maxTimeCs，中间=$midTimeCs,地址=$chipAddress" group="PCA9685"  advanced=true
+    export function setServoLimits(servoNum: PinNum = 0, minTimeCs: number = 5, maxTimeCs: number = 2.5, midTimeCs: number = -1, chipAddress: number = 0x40): void {
+        const chip = getChipConfig(chipAddress)
+        servoNum = Math.max(1, Math.min(16, servoNum))
+        minTimeCs = Math.max(0, minTimeCs)
+        maxTimeCs = Math.max(0, maxTimeCs)
+        debug(`setServoLimits(${servoNum}, ${minTimeCs}, ${maxTimeCs}, ${chipAddress})`)
+        const servo: ServoConfig = chip.servos[servoNum - 1]
+        midTimeCs = midTimeCs > -1 ? midTimeCs : ((maxTimeCs - minTimeCs) / 2) + minTimeCs
+        debug(`midTimeCs ${midTimeCs}`)
+        return servo.setOffsetsFromFreq(minTimeCs, maxTimeCs, midTimeCs)
+    }
+
+    /**
+     * Used to setup the chip, will cause the chip to do a full reset and turn off all outputs.
+     * @param chipAddress [64-125] The I2C address of your PCA9685; eg: 64
+     * @param freq [40-1000] Frequency (40-1000) in hertz to run the clock cycle at; eg: 50
+     */
+    //% block="初始化模块,地址=$chipAddress" group="PCA9685"  advanced=true
+    export function init(chipAddress: number = 0x40, newFreq: number = 50) {
+        debug(`Init chip at address ${chipAddress} to ${newFreq}Hz`)
+        const buf = pins.createBuffer(2)
+        const freq = (newFreq > 1000 ? 1000 : (newFreq < 40 ? 40 : newFreq))
+        const prescaler = calcFreqPrescaler(freq)
+
+        write(chipAddress, modeRegister1, sleep)
+
+        write(chipAddress, PrescaleReg, prescaler)
+
+        write(chipAddress, allChannelsOnStepLowByte, 0x00)
+        write(chipAddress, allChannelsOnStepHighByte, 0x00)
+        write(chipAddress, allChannelsOffStepLowByte, 0x00)
+        write(chipAddress, allChannelsOffStepHighByte, 0x00)
+
+        write(chipAddress, modeRegister1, wake)
+
+        control.waitMicros(1000)
+        write(chipAddress, modeRegister1, restart)
+    }
+
+    /**
+     * Used to reset the chip, will cause the chip to do a full reset and turn off all outputs.
+     * @param chipAddress [64-125] The I2C address of your PCA9685; eg: 64
+     */
+    //% block="重置模块,地址=$chipAddress" group="PCA9685" 
+    export function reset(chipAddress: number = 0x40): void {
+        return init(chipAddress, getChipConfig(chipAddress).freq);
+    }
+
+    /**
+     * Used to reset the chip, will cause the chip to do a full reset and turn off all outputs.
+     * @param hexAddress The hex address to convert to decimal; eg: 0x40
+     */
+    //% block="设置模块地址为$chipAddress" group="PCA9685"
+    export function chipAddress(hexAddress: string): number {
+        hexAddress = stripHexPrefix(hexAddress)
+        let dec = 0
+        let lastidx = 0
+        let lastchar = 0
+        const l = Math.min(2, hexAddress.length)
+        for (let i = 0; i < l; i++) {
+            const char = hexAddress.charAt(i)
+            const idx = hexChars.indexOf(char)
+            const pos = l - i - 1
+            lastidx = pos
+            dec = dec + (idx * Math.pow(16, pos))
+        }
+        return dec
+    }
+
+    export function setDebug(debugEnabled: boolean): void {
+        _DEBUG = debugEnabled
+    }
+}
